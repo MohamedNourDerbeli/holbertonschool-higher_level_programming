4cc3ba11da89c2e24f47a0fc8391657852a346de
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-22 06:06:07 +0100

    program will assign a random signed number to the variable number each time it is executed.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 10):
         if i != k and i < k and (str(i) + str(k)) != "89":
             print("{},".format(str(i) + str(k)), end=" ")
         if (str(i) + str(k)) == "89":
-            print("{} ".format(str(i)+str(k)))
+            print("{}".format(str(i)+str(k)))
