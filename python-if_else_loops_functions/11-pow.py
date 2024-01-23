06c4c47dadde716d3a0f4ed3e81c2ef8613c8014
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-23 07:29:13 +0100

    POW  of two number py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return (a ^ b)
+    if b == 0:
+        return 1
+    if a < 0 and b % 2 == 0:
+        return (-1) * (abs(a) ** b)
+    else:
+        return a ** b
