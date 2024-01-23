67c8598f5e987cd0cdf1cdc17b680acf8f9f55fb
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-23 07:30:36 +0100

    POW  of two number py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def pow(a, b):
+    if a == 0 and b < 0:
+        raise ValueError("Undefined result: 0^(-b) where b is negative.")
     if b == 0:
         return (1)
     if a < 0 and b % 2 == 0:
