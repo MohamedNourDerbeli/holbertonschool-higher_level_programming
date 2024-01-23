0caf732ce38572a34077af53fdeaa67de334aaf1
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-23 09:54:31 +0100

    program that prints the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,9 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
     s = 0
-    if len(argv) <= 1:
-        print("{}".format(len(argv)-1))
-        exit
-    for i in range(1, len(argv)):
-        s += int(argv[i])
-    print("{}".format(s))
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            s += int(argv[i])
+        print("{}".format(s))
+    print("{}".format(len(argv)-1))
