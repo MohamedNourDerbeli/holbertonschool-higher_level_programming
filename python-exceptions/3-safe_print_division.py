d89dde286e782fc3b4030e5bbde5de57e033d1c3
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-30 23:52:27 +0100

    function that divides 2 integers and prints the result.

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         res = a / b
         print("Inside result: {}".format(res))
-    except ():
+    except ZeroDivisionError:
         res = None
         print("Inside result: {}".format(res))
     finally:
