ade9dbbe72be4bf6cda5715daa7c330d2c6486ff
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-31 14:13:42 +0100

    an class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This class represents an empty square.
+
+Attributes:
+    _size (int): The size of each side of the square.
+"""
+
+
+class Square:
+    """
+    This class represents a emty square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square object with the given size.
+        """
+        self._Square__size = size
+        pass
