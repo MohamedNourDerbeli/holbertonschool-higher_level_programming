115145466b351592ec3a61dc12b34c7b11dda248
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-03 18:50:21 +0100

    Unittest for max_integer([..])

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -17,7 +17,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-1, -25, -6]), -1)
         self.assertEqual(max_integer([-1, -25, 11, 11]), 11)
-        self.assertRaises(Exception, max_integer,["str",1])
+        self.assertRaises(Exception, max_integer,["str", 39, {4}, 2,3])
 
 if __name__ == "__main__":
     unittest.main()
