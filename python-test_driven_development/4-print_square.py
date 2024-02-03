b4d136861d845d73f96a086fe627fb63276cde15
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-03 13:49:33 +0100

    script defines a function 'print_square' that prints a square made of '#' characters with the specified size.

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script defines a function 'print_square' that prints a
+square made of '#' characters with the specified size.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square made of '#' characters with the specified size.
+
+    Parameters:
+    - size (int or float): The size of the square.
+
+    Raises:
+    - TypeError: If size is not an integer or if it's a negative float.
+
+    Returns:
+    - None
+    """
+
+    # Check if size is an integer or a non-negative float
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be a non-negative integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be a non-negative integer")
+    # Check if size is non-negative
+    if size < 0:
+        raise TypeError("size must be >= 0")
+
+    # Print the square
+    for _ in range(size):
+        print("#" * size)
