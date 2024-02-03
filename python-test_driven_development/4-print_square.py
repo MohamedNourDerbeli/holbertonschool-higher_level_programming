f9603b31a662d74d31455551993099205a92a0d0
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-03 14:04:29 +0100

    script defines a function 'print_square' that prints a square made of '#' characters with the specified size.

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -21,9 +21,9 @@ def print_square(size):
 
     # Check if size is an integer or a non-negative float
     if not isinstance(size, (int, float)):
-        raise TypeError("size must be a non-negative integer")
+        raise TypeError("size must be an integer")
     if type(size) is float and size < 0:
-        raise TypeError("size must be a non-negative integer")
+        raise TypeError("size must be an integer")
     # Check if size is non-negative
     if size < 0:
         raise TypeError("size must be >= 0")
