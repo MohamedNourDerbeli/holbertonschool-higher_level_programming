9197334317fdecd1d45841a2519b98752a6a93af
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-03 19:25:42 +0100

    Unittest for max_integer([..])

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
 """
-    unittest
+    Unittest for max_integer([..])
 """
-
-
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+
+# Importing the max_integer function from the module to be tested
+max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """ class for unittests """
-    def test_max_integer(self):
-        """ check possible cases edge cases """
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([3]), 3)
-        self.assertEqual(max_integer([-3]), -3)
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
-        self.assertEqual(max_integer([15, 10, 5]), 15)
-        self.assertEqual(max_integer([15, 10, 5, -5, -10, 15]), 15)
-        self.assertEqual(max_integer([15, 15, 15]), 15)
+    """Test cases for the max_integer function."""
 
-    def test_type_error(self):
-        """ type_errors """
-        self.assertRaises(TypeError, max_integer, ["h", 1])
-        self.assertRaises(TypeError, max_integer, [2, [2, 1]])
+    def test_all(self):
+        """Test case"""
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1.34]), 1.34)
+        self.assertEqual(max_integer([-5]), -5)
+        self.assertEqual(max_integer([11, 1]), 11)
+        self.assertEqual(max_integer([91, 41, 21]), 91)
+        self.assertEqual(max_integer([11, 11, 11]), 11)
+        self.assertEqual(max_integer([-1, -25, -6]), -1)
+        self.assertEqual(max_integer([-1, -25, 11, 11]), 11)
+        self.assertEqual(max_integer([-1, -25, 0.1, 11.4]), 11.4)
+        self.assertEqual(max_integer(list(range(100000))), 99999)
+
+    def test_emty_list(self):
+        """Emty list"""
+        self.assertIsNone(max_integer())
+
+    def test_error(self):
+        """Error case"""
+        self.assertRaises(TypeError, max_integer, ["r", 1])
+        self.assertRaises(TypeError, max_integer, [23, [11, 23]])
