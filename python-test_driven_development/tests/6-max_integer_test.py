4ccbf4f499ed6e430326da68b9122c9b973de577
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-03 19:04:38 +0100

    Unittest for max_integer([..])

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,6 +6,7 @@ import unittest
 # Importing the max_integer function from the module to be tested
 max_integer = __import__("6-max_integer").max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Test cases for the max_integer function."""
 
@@ -13,14 +14,21 @@ class TestMaxInteger(unittest.TestCase):
         """Test case"""
         self.assertEqual(max_integer([11, 1]), 11)
         self.assertEqual(max_integer([1]), 1)
-        self.assertIsNone(max_integer())
+        self.assertEqual(max_integer([-1]), -1)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-1, -25, -6]), -1)
         self.assertEqual(max_integer([-1, -25, 11, 11]), 11)
+        self.assertEqual(max_integer([11, 11, 11]), 11)
+
+    def test_emty_list(self):
+        """Emty list"""
+        self.assertIsNone(max_integer())
 
     def test_error(self):
         """Error case"""
-        self.assertRaises(TypeError, max_integer,["str", 39, {4}, 2,3])
-        self.assertRaises(TypeError, max_integer,[23, 11],[11, 23])
+        self.assertRaises(TypeError, max_integer, ["str", 39, {4}, 2, 3])
+        self.assertRaises(TypeError, max_integer, [23, [11, 23]])
+
+
 if __name__ == "__main__":
     unittest.main()
