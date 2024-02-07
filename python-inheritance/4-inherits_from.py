93cb39f6fa1da1b9d7b7f921408f3a8d3ac28375
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-07 15:24:21 +0100

    function that returns True if the object is an instance of a class that inherited

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """
+    Checks if the given object is a subclass of
+    the specified class.
+    """
+    return isinstance(int(obj), a_class)
