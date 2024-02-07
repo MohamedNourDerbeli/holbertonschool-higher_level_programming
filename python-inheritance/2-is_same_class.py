cffbcd13a2b74c058393a4b5258edc79a5809ff2
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-07 15:06:11 +0100

    This function checks if the given object is an instance of the specified class.

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+"""
+This function checks if the given object
+is an instance of the specified class.
+"""
+
+
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """
+    If they match, the function returns True;
+    otherwise, it returns False.
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
