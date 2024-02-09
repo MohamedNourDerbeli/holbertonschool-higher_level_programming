2e8c08ae52cd799104934e01b9b3cb619b388c00
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-09 21:04:18 +0100

    Defines a base geometry class BaseGeometry

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Author: Nour Mohamed
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """A class representing a square. Inherits from Rectangle."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
