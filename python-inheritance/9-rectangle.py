8b7ed2d16492eff26ea8815cf94094b2fca9a189
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-09 20:42:13 +0100

    Defines a base geometry class BaseGeometry

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a base geometry class BaseGeometry."""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.__width * self.__height
