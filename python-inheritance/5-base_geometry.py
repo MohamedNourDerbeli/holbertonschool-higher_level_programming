847e50fbdf80d52f751f97930a9ea789d0e80fea
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-09 18:09:35 +0100

    It is currently empty and serves as a foundation for     other classes that will inherit from it.

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr//python3
+
+"""
+This is a base class for geometry-related objects.
+"""
+
+
+class BaseGeometry:
+    """
+    It is currently empty and serves as a foundation for
+    other classes that will inherit from it.
+    """
+
+    pass
