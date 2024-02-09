4050a906d30bcf59c040392b4c146b14370b862e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-09 18:27:01 +0100

    This is a base class for geometry-related objects.

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This is a base class for geometry-related objects.
+"""
+
+
+class BaseGeometry:
+    """
+    This is the base class for all geometry-related classes in this package.
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
