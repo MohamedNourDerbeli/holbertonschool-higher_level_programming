ddb03a0c4ca0d1dbb7c2bedd35a9507fbe0298dc
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-09 20:33:32 +0100

    Defines a base geometry class BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle(BaseGeometry):
     """
 
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
