7b31436049a60df1c4ce7060a7b047f25ea1e90b
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-09 20:53:28 +0100

    Defines a base geometry class BaseGeometry

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         super().__init__(size, size)
-        self.size = size
+        self.__size = size
 
     def area(self):
-        return self.size * self.size
+        return self.__size *self.__size
