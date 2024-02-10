0c301c8356a9e25f665ec3d60830feeef59915ec
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-10 14:01:50 +0100

    function that returns an object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that returns an object """
+import json
+
+
+def from_json_string(my_str):
+    """serialize the object from a JSON formatted string"""
+
+    return json.load(my_str)
