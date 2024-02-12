4530849fbd13b08d2f72698966e6ec208b5b9f0d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-12 15:54:32 +0100

    class Square that inherits from Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -43,4 +43,13 @@ class Square(Rectangle):
         self.width = self.height = value
 
     def update(self, *args, **kwargs):
-        Rectangle.update(self, *args, **kwargs)
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
