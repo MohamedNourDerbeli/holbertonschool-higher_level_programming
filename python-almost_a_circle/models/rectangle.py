7af2f60434990248f94e36ced4d0cd045413870b
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-12 14:22:13 +0100

    Update the class Rectangle by improving the public method def display(self)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,16 @@ class Rectangle(Base):
         """Return a string representation of the rectangle."""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
 {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """Update one or more attributes of the rectangle."""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__x = args[4]
