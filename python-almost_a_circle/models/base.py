d927931b4010c26bde8328da17d4e5af770bb8af
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-12 10:40:54 +0100

    class Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+This is a simple script to demonstrate
+how to use the pyrebase library.
+"""
+
+
 class Base:
+    """A base class for our Firebase application."""
+
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             self.__nb_objects += 1
-            self.id =  self.__nb_objects
+            self.id = self.__nb_objects
