19a08854fd3d64b613d79fcad98684160a141e5b
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-17 20:58:20 +0100

    fix error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -31,7 +31,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter method for the width attribute."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
@@ -45,7 +45,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter method for the x coordinate attribute."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value < 0:
             raise ValueError("x must be >= 0")
@@ -59,7 +59,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter method for the y coordinate attribute."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         elif value < 0:
             raise ValueError("y must be >= 0")
@@ -95,13 +95,13 @@ class Rectangle(Base):
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
-            self.__width = args[1]
+            self.width = args[1]
         if len(args) >= 3:
-            self.__height = args[2]
+            self.height = args[2]
         if len(args) >= 4:
-            self.__x = args[3]
+            self.x = args[3]
         if len(args) >= 5:
-            self.__y = args[4]
+            self.y = args[4]
         for key, value in kwargs.items():
             setattr(self, key, value)
 
