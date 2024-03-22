eebc58102eccae388ae56b788c447520832231ba
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-22 21:11:31 +0100

    script that takes in an argument and displays  all values in the states if it matches

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,6 +7,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
