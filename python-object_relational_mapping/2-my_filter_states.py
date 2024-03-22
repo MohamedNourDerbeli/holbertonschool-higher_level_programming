5591cad4fb971ac5d4924523176c173a19442d95
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-22 21:12:56 +0100

    script that takes in an argument and displays  all values in the states if it matches

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
- script that lists all states with a
- name starting with N
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
+
 import MySQLdb
 from sys import argv
 
@@ -11,9 +14,17 @@ if __name__ == "__main__":
     Access to the database and get the states
     from the database.
     """
+
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]
     )
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    [print(state) for state in cur.fetchall() if state[1] == argv[4]]
+    cur.execute(
+        "SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(
+            argv[4]
+        )
+    )
+    [print(state) for state in cur.fetchall()]
