816e1597cd4bb5ee5052e2add5df52fcb5364551
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-22 21:04:07 +0100

    script that takes in an argument and displays  all values in the states if it matches

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,5 +11,7 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    [print(state) for state in cur.fetchall() if state[1] == argv[4]]
+    cur.execute(f"SELECT * FROM states \
+                 WHERE name LIKE BINARY '{argv[4]}' \
+                 ORDER BY states.id ASC")
+    [print(state) for state in cur.fetchall()]
