d385a90e8eac95b6c4a4b563e2d27688ece7d97e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-22 21:29:48 +0100

    script that takes in an argument and displays  all values in the states if it matches

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
     """
 
-    db = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute(
-        """
+    with db.cursor() as cur:
+        cur.execute("""
             SELECT
                 *
             FROM
@@ -30,7 +26,12 @@ if __name__ == "__main__":
                 name LIKE BINARY %(name)s
             ORDER BY
                 states.id ASC
-        """,
-        {"name": argv[4]},
-    )
-    [print(state) for state in cur.fetchall() if cur.fetchall() is not None]
+        """, {
+            'name': argv[4]
+        })
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
