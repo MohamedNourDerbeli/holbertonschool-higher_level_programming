4929d766128fa02449f66766026047e90a223ce9
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-23 12:40:26 +0100

    script that takes in an argument and displays

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,9 @@ from the database `hbtn_0e_0_usa`.
 
 import MySQLdb
 import sys
-def  main(username, passsword, db_name, state_name):
+
+
+def main(username, passsword, db_name, state_name):
     """
     Access to the database and get the states
     from the database.
@@ -33,6 +35,8 @@ def  main(username, passsword, db_name, state_name):
         {"name": state_name},
     )
     [print(state) for state in cur.fetchall()]
+
+
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
