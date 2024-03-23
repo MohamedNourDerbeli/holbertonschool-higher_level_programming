ff7624b9c398cfb3fc91eaaec871cc6d05e2c687
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-23 13:42:24 +0100

    lists all cities of that state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+lists all cities of that state, using the
+database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+
+def main(username, passsword, db_name, state_name):
+    """
+    lists all cities of that state
+    """
+
+    db = MySQLdb.connect(
+        host="localhost", user=username,
+        port=3306, passwd=passsword, db=db_name
+    )
+    cur = db.cursor()
+
+    cur.execute(
+        """
+        SELECT cities.name FROM \
+cities JOIN states ON \
+cities.state_id = states.id \
+WHERE states.name = %(name)s ORDER BY \
+cities.id ASC;
+        """,
+        {"name": state_name},
+    )
+    results = cur.fetchall()
+    li = list()
+    for row in results:
+        li.append(row[0])
+    li = set(li)
+    print(", ".join(li))
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    main(username, password, db_name, state_name)
